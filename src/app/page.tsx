2d26b166846edf79ba171aff485236ef38d6ce46
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-15 22:32:59 +0530

    added div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import Image from "next/image";
@@ -8,26 +9,41 @@ let weatherApiKey = "8f6200216e7a219e044fb1179fea87b6";
 
 export default function Home() {
   const [place, setPlace] = useState("Mumbai");
+  const [placeData, setPlaceData] = useState<any>(null);
+  
   const getWeatherData = async () => {
     if (place && place.length > 0) {
       try {
         let url = `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${weatherApiKey}`;
         let res = await fetch(url);
         let data = await res.json();
-        console.log(data);
+        setPlaceData(data)
       } catch (err) {
         console.log(err);
       }
     }
   };
-  useEffect(() => {}, []);
-  getWeatherData();
+  useEffect(() => {  getWeatherData();
+
+  }, [place]);
+ 
   return (
     <div className="h-screen bg-[url('https://images.unsplash.com/photo-1601225691237-b0a1dfa036e0?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center flex flex-col items-center">
       <div className="flex bg-[rgba(0,0,0,0.25)] px-2 py-2 items-center rounded-2xl mt-10">
-        <input className="bg-transparent outline-none text-white p-2" type="search" placeholder="City Name" onChange={(e)=>setPlace(e.target.value)} />
+        <input className="bg-transparent outline-none text-white p-2" type="search" placeholder="City Name" onChange={(e) => setPlace(e.target.value)} />
         <button className="flex items-center text-gray-500 gap-2" onClick={getWeatherData}>Search<IoSearch /></button>
       </div>
+      {placeData && (
+        <div className="row">
+          <div className="section1">
+            <div className="section11"></div>
+            <div className="section11"></div>
+          </div>
+          <div className="time-div">
+            <p className="time"></p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
