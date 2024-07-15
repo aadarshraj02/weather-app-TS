da4947b106d1b7c96fa868a7e93d712752f53af8
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-15 19:22:24 +0530

    fetched weather dat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,17 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-let weatherApiKey =process.env.NEXT_PUBLIC_WEATHER_API_KEY
+let weatherApiKey = "8f6200216e7a219e044fb1179fea87b6";
 
 export default function Home() {
   const [place,setPlace] = useState("Mumbai");
   const getWeatherData = async ()=>{
     if(place && place.length>0){
       try{
-        let url =  `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${weatherApiKey}`
+        let url =  `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${weatherApiKey}`;
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data);
       }
       catch(err){
         console.log(err)
