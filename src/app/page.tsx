d0ecd8a5b5f271aec34e499328db58cc24a58bb7
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-15 21:29:04 +0530

    made whole div flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,6 @@ export default function Home() {
   useEffect(() => {}, []);
   getWeatherData();
   return (
-    <div className="h-screen bg-[url('https://images.unsplash.com/photo-1601225691237-b0a1dfa036e0?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center"></div>
+    <div className="h-screen bg-[url('https://images.unsplash.com/photo-1601225691237-b0a1dfa036e0?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center flex flex-col items-center"></div>
   );
 }
