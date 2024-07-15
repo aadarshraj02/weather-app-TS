ed54a2cdded81f7b9d7000d9b287cc5a4e2c4078
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-15 21:27:48 +0530

    added bg image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,23 +6,22 @@ import { useEffect, useState } from "react";
 let weatherApiKey = "8f6200216e7a219e044fb1179fea87b6";
 
 export default function Home() {
-  const [place,setPlace] = useState("Mumbai");
-  const getWeatherData = async ()=>{
-    if(place && place.length>0){
-      try{
-        let url =  `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${weatherApiKey}`;
+  const [place, setPlace] = useState("Mumbai");
+  const getWeatherData = async () => {
+    if (place && place.length > 0) {
+      try {
+        let url = `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${weatherApiKey}`;
         let res = await fetch(url);
         let data = await res.json();
         console.log(data);
-      }
-      catch(err){
-        console.log(err)
+      } catch (err) {
+        console.log(err);
       }
     }
-  }
-  useEffect(()=>{},[])
+  };
+  useEffect(() => {}, []);
   getWeatherData();
   return (
-   <div></div>
+    <div className="h-screen bg-[url('https://images.unsplash.com/photo-1601225691237-b0a1dfa036e0?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center"></div>
   );
 }
