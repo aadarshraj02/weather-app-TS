83f007e513edda9dc5cd64f8995e2e5fc8e95a4b
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-15 19:05:32 +0530

    added const to set default place

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [place,setPlace] = useState("Mumbai");
+
   return (
    <div></div>
   );
